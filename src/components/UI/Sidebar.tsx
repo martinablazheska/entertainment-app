79ed51c50be4048fda53b8d9f605c0b431c963ef
Martina Blazheska <123024212+martinablazheska@users.noreply.github.com>
2023-12-15 18:37:49 +0100

    Style changes to sidebar

--- src/components/UI/Sidebar.tsx
+++ src/components/UI/Sidebar.tsx
@@ -14,24 +14,26 @@ import classes from "./Sidebar.module.scss";
 function Sidebar() {
   return (
     <div className={classes.sidebar}>
-      <div className={classes.logo}>
-        <Link to="/">
-          <img src={logo} alt="logo" />
-        </Link>
-      </div>
-      <div className={classes.navigation}>
-        <NavLink to="/">
-          <img src={homeIcon} alt="Home" />
-        </NavLink>
-        <NavLink to="/movies">
-          <img src={movieIcon} alt="Movies" />
-        </NavLink>
-        <NavLink to="/series">
-          <img src={seriesIcon} alt="TV Series" />
-        </NavLink>
-        <NavLink to="/bookmarked">
-          <img src={bookmarkIcon} alt="Bookmarks" />
-        </NavLink>
+      <div className={classes.links}>
+        <div className={classes.logo}>
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+        </div>
+        <div className={classes.navigation}>
+          <NavLink to="/">
+            <img src={homeIcon} alt="Home" />
+          </NavLink>
+          <NavLink to="/movies">
+            <img src={movieIcon} alt="Movies" />
+          </NavLink>
+          <NavLink to="/series">
+            <img src={seriesIcon} alt="TV Series" />
+          </NavLink>
+          <NavLink to="/bookmarked">
+            <img src={bookmarkIcon} alt="Bookmarks" />
+          </NavLink>
+        </div>
       </div>
       <div className={classes.avatar}>
         <img src={avatar} alt="User avatar" />
